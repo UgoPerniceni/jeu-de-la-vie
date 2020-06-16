b144825629fefa5476a7f739b6aa7d3b4a7475dd
Kelig Martin <martin.kelig@gmail.com>
2020-06-16 17:24:15 +0200

    refacto Cell and Cells file :sparkles:

--- models/Cell.py
+++ models/Cell.py
@@ -1,20 +1,20 @@
 class Cell:
     id = 0
 
-    def __init__(self, x, y, alive=False, cellSize=10):
+    def __init__(self, x, y, alive=False, cell_size=10):
         self.row = x
         self.col = y
 
-        self.x = x * cellSize
-        self.y = y * cellSize
+        self.x = x * cell_size
+        self.y = y * cell_size
 
         self.alive = alive
 
         Cell.id += 1
         self.id = Cell.id
 
-    def setAlive(self):
+    def set_alive(self):
         self.alive = True
 
-    def toString(self):
+    def to_string(self):
         return 'Cell n°{} (Cells[r: {}, c: {}], Position (X={}, Y={}),alive = {})'.format(self.id, self.row, self.col, self.x, self.y, self.alive)
